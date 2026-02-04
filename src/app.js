a1591ac44619bc1eec2ef22ae90a14ac2adbb8ee
Anshu Paliwal <paliwalanshu35@gmail.com>
2026-02-04 21:41:32 +0530

    changes

--- src/app.js
+++ src/app.js
@@ -2,7 +2,7 @@ const express = require("express");
 
 const app = express();
 
-// this will only handle get calls to /user with parameters userId, name, and password
+// this will only handle get calls to /user with parameters userId, name, and password changes
 app.get("/user/:userId/:name/:password", (req, res) => {
   console.log(req.params);
   res.send({ firstName: "John", lastName: "Doe" });
