5d8bdc2afaf3c96aab0d4bcaf9e768168ee9c9de
Anshu Paliwal <paliwalanshu35@gmail.com>
2026-02-05 17:07:23 +0530

    multiple routes push

--- src/app.js
+++ src/app.js
@@ -3,10 +3,18 @@ const express = require("express");
 const app = express();
 
 // this will only handle get calls to /user with parameters userId, name, and password changes
-app.get("/user/:userId/:name/:password", (req, res) => {
-  console.log(req.params);
-  res.send({ firstName: "John", lastName: "Doe" });
-});
+app.get(
+  "/user",
+  (req, res, next) => {
+    console.log("handling the route user!!");
+    next();
+  },
+
+  (req, res) => {
+    console.log("handling the route user");
+    res.send("User route accessed");
+  },
+);
 
 app.listen(7777, () => {
   console.log("Server is running on port 7777");
