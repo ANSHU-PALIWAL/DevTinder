9a81bab82520712aaaf6fc70e439d9c114ff9f46
Anshu Paliwal <paliwalanshu35@gmail.com>
2026-02-17 16:26:15 +0530

    Api level Data Sanitization

--- src/app.js
+++ src/app.js
@@ -54,16 +54,33 @@ app.delete("/user", async (req, res) => {
 });
 
 // Update data of the user
-app.patch("/user", async (req, res) => {
-  const userId = req.body.userId;
+app.patch("/user/:userId", async (req, res) => {
+  const userId = req.params?.userId;
   const data = req.body;
+
   try {
-    await User.findByIdAndUpdate({ _id: userId }, data, {
+    const ALLOWED_UPDATES = ["photoUrl", "about", "gender", "age", "skills"];
+
+    const isUpdateAllowed = Object.keys(data).every((k) =>
+      ALLOWED_UPDATES.includes(k),
+    );
+
+    if (!isUpdateAllowed) {
+      throw new Error("Update Not Alowed");
+    }
+
+    if (data?.skills.length > 10) {
+      throw new Error("Skills cannot be more than 10");
+    }
+
+    const user = await User.findByIdAndUpdate({ _id: userId }, data, {
       returnDocument: "after",
+      runValidators: true,
     });
+    console.log(user);
     res.send("User Updated successfully");
   } catch (err) {
-    res.status(400).send("Something Went Wrong");
+    res.status(400).send("Update Failed:" + err.message);
   }
 });
 
--- src/models/user.js
+++ src/models/user.js
@@ -1,24 +1,65 @@
 const mongoose = require("mongoose");
 
-const userSchema = new mongoose.Schema({
-  firstName: {
-    type: String,
+const userSchema = new mongoose.Schema(
+  {
+    firstName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    lastName: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+    },
+    emailId: {
+      type: String,
+      required: true,
+      lowercase: true,
+      unique: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+    age: {
+      type: Number,
+      required: true,
+      min: 18,
+    },
+    gender: {
+      type: String,
+      required: true,
+      validate(value) {
+        if (!["male", "female", "others"].includes(value)) {
+          throw new Error("Gender Is Not Valid");
+        }
+      },
+    },
+    photoUrl: { 
+      type: String,
+      required: true,
+      default:
+        "https://cdn.vectorstock.com/i/500p/46/76/gray-male-head-placeholder-vector-23804676.jpg",
+    },
+    about: {
+      type: String,
+      required: true,
+      minLength: 3,
+      maxLength: 50,
+      default: "This is a defualt about of the user",
+    },
+    skills: {
+      required: true,
+      type: [String],
+    },
   },
-  lastName: {
-    type: String,
+  {
+    timestamps: true,
   },
-  emailId: {
-    type: String,
-  },
-  password: {
-    type: String,
-  },
-  age: {
-    type: Number,
-  },
-  gender: {
-    type: String,
-  },
-});
+);
 
 module.exports = mongoose.model("User", userSchema);
