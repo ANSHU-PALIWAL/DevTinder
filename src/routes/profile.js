0207083b786df0ac34c53ff29a0127368b7ecb93
Anshu Paliwal <paliwalanshu35@gmail.com>
2026-03-05 18:33:58 +0530

    created delete and password update functio ality

--- src/routes/profile.js
+++ src/routes/profile.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const bcrypt = require("bcrypt");
 const { userAuth } = require("../middlewares/auth");
 const { validateEditProfileData } = require("../utils/validation");
 
@@ -55,6 +56,49 @@ profileRouter.patch("/profile/edit-password", userAuth, async (req, res) => {
     });
   } catch (err) {
     res.status(400).send("Error: " + err.message);
+  }
+});
+
+// UPDATE PASSWORD API
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { currentPassword, newPassword } = req.body;
+    const loggedInUser = req.user;
+
+    // Verify current password
+    const isPasswordValid = await bcrypt.compare(
+      currentPassword,
+      loggedInUser.password,
+    );
+    if (!isPasswordValid) {
+      return res.status(400).send("Incorrect current password.");
+    }
+
+    // Hash the new password
+    const passwordHash = await bcrypt.hash(newPassword, 10);
+
+    // Save to database
+    loggedInUser.password = passwordHash;
+    await loggedInUser.save();
+
+    res.json({ message: "Password updated successfully!" });
+  } catch (err) {
+    res.status(500).send("Error updating password: " + err.message);
+  }
+});
+
+// DELETE ACCOUNT API
+profileRouter.delete("/profile/delete", userAuth, async (req, res) => {
+  try {
+    // 💥 FIX: We use req.user.deleteOne() so we don't need to import the User model!
+    await req.user.deleteOne();
+
+    // Clear the auth cookie so they are logged out
+    res.cookie("token", null, { expires: new Date(Date.now()) });
+
+    res.json({ message: "Account deleted successfully." });
+  } catch (err) {
+    res.status(500).send("Error deleting account: " + err.message);
   }
 });
 
